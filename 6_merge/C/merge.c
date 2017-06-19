59ca355536122a1665e35dc74ea742a2e05397f5
Helio Albano de Oliveira <helioalb@gmail.com>
2017-06-18 22:18:49 -0300

    C implementation of merge

---
+++ 6_merge/C/merge.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void merge(int* A, int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int n3 = n1 + n2;
+    int i = p;
+    int j = q + 1;
+    int k = 0;
+    int* B = calloc(n3, sizeof (int));
+
+    while (i <= q && j <= r)
+        if (A[i] < A[j])
+            B[k++] = A[i++];
+        else
+            B[k++] = A[j++];
+
+    while (k < n3)
+        if (i > q)
+            B[k++] = A[j++];
+        else
+            B[k++] = A[i++];
+
+    for (i = 0; i < n3; i++)
+        A[p++] = B[i];
+}
+
+void print_array(int* A, int size) {
+    int i;
+    
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+int main() {
+    int A[8] = { 1, 3, 5, 6, 2, 4, 7, 8 };
+    int B[15] = { 9, 9, 9, 1, 3, 5, 6, 2, 4, 7, 8, 9, 9, 9, 9 };
+    int C[8] = { 2, 4, 7, 8, 1, 3, 5, 6 };
+    int D[15] = { 9, 9, 9, 2, 4, 7, 8, 1, 3, 5, 6, 9, 9, 9, 9 }; 
+
+    merge(A, 0, 3, 7);
+    print_array(A, 8);
+
+    merge(B, 3, 6, 10);
+    print_array(B, 15);
+
+    merge(C, 0, 3, 7);
+    print_array(A, 8);
+
+    merge(D, 3, 6, 10);
+    print_array(B, 15);
+
+    return 0;
+}

c9e0f4cdf437bb23248e4572829d7376245a8b32
Helio Albano de Oliveira <helioalb@gmail.com>
2017-06-18 22:32:15 -0300

    Add test cases

--- 6_merge/C/merge.c
+++ 6_merge/C/merge.c
@@ -37,7 +37,9 @@ int main() {
     int B[15] = { 9, 9, 9, 1, 3, 5, 6, 2, 4, 7, 8, 9, 9, 9, 9 };
     int C[8] = { 2, 4, 7, 8, 1, 3, 5, 6 };
     int D[15] = { 9, 9, 9, 2, 4, 7, 8, 1, 3, 5, 6, 9, 9, 9, 9 }; 
-
+    int E[1] = { 1 };
+    int F[4] = { 1, 3, 2, 4 };
+    int G[4] = { 1, 2, 3, 4 };
     merge(A, 0, 3, 7);
     print_array(A, 8);
 
@@ -45,10 +47,20 @@ int main() {
     print_array(B, 15);
 
     merge(C, 0, 3, 7);
-    print_array(A, 8);
+    print_array(C, 8);
 
     merge(D, 3, 6, 10);
-    print_array(B, 15);
+    print_array(D, 15);
+
+    merge(E, 0, 0, 0);
+    print_array(E, 1);
+
+    merge(F, 0, 1, 3);
+    print_array(F, 4);
+
+    merge(G, 0, 1, 3);
+    print_array(G, 4);
+
 
     return 0;
 }
